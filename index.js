279a5c45f7007db9f800f195085f2b49d537d291
Woolim Plant <Osman Gani>
2023-11-28 17:50:29 +0600

    the payment data has been colect from databage

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -31,6 +32,98 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("sboEmployeeManagement").collection("users");
+    const paymentCollection = client.db("sboEmployeeManagement").collection("payment");
+
+
+    // all user collect for HR sent to clinet employeee list
+
+    app.get('/users', async (req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      });
+
+
+      // payment collect colect from database
+
+
+      app.get('/payments/:email', async (req, res) => {
+        const query = { email: req.params.email }
+        const result = await paymentCollection.find(query).toArray();
+        res.send(result);
+      })
+
+
+
+//    ------------userCollection-----------------
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        // insert email if user doesnt exists: 
+        // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      });
+
+
+      // update statust is verified
+
+
+      app.patch('/users/:id', async(req,res) =>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const verified = req.body;
+        console.log(verified);
+        const updateDoc = {
+          $set: {
+            isVerfied: verified.isVerfied
+          }
+        }
+      
+        const result = await userCollection.updateOne(filter, updateDoc)
+        res.send(result)
+       })
+
+
+
+      //  payment related method
+
+
+      app.post('/create-payment-intent', async (req, res) => {
+        const { salary } = req.body;
+        const amount = parseInt(salary * 100);
+        console.log(amount, 'amount inside the intent')
+  
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'BDT',
+          payment_method_types: ['card']
+        });
+  
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
+      });
+
+
+      // payment save to data baseURL:
+      
+
+      app.post('/payments', async (req, res) => {
+        const payment = req.body;
+        const paymentResult = await paymentCollection.insertOne(payment);
+        res.send(paymentResult)
+      })
+  
+       
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
